e3b7071eeb52f7da37534c32c938f769102c7a24
DeepaJeyshnitha <99550892+DeepaJeyshnitha@users.noreply.github.com>
2022-02-12 23:55:30 +0530

    Update frontend.js

--- js/frontend.js
+++ js/frontend.js
@@ -1,13 +1,11 @@
 const manage_html = {
 	make_html:function(){
-		var ajax_url='/./Mock_test_1/php/backend.php?XDEBUG_SESSION_START=sublime.xdebug';
+		// var ajax_url='/./Mock_test_1/php/backend.php?XDEBUG_SESSION_START=sublime.xdebug';
+		var ajax_url='php/backend.php';
 		var ajax_data={};
 		var table_div_id='#main_div';
 		var input_value='';
-		var data_per_page=10;
-		var search_enable_or_desiable=true;
-		var type='data';
-		make_table_pagination(ajax_url,ajax_data,table_div_id,data_per_page,search_enable_or_desiable,input_value);
+	@@ -11,4 +12,4 @@ const manage_html = {
 	}
 }
 let manage_html_obj = Object.create(manage_html);
